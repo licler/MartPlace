056c04f6588d8acf38b5f0aa0c2c96659730adf0
licler <ute3@yandex.ru>
2020-09-15 21:51:15 +0300

    added rotayo

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,17 @@
 $(function(){
  
 
+      $(".rate-star").rateYo({
+        rating: 4.5,
+        starWidth: '13px'
+      });
+
+
+
+      $('.featured__item').slick({
+        
+      });
+
     var mixer = mixitup('.products__inner-box');
 
 
