081e0da28af634279b1bebdcd8eb807464914fb3
licler <ute3@yandex.ru>
2020-09-19 21:03:02 +0300

    added works, news, footer, copi

--- app/js/main.js
+++ app/js/main.js
@@ -8,10 +8,26 @@ $(function(){
 
 
 
-      // $('.').slick({
-      //   arrows: false,
-      //   variableWidth: true
-      // });
+      $('.featured__slider').slick({
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/featured/previous.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/featured/next.png" alt=""></button>'
+      });
+      $('.followers__inner-box').slick({
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/featured/previous.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/featured/next.png" alt=""></button>'
+      });
+
+      $('.clients__slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/featured/previous.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/featured/next.png" alt=""></button>'
+      });
 
     var mixer = mixitup('.products__inner-box');
 
